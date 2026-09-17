7db6e574029e065547f27169d7de23dcfe8a941c
Ricardo Mora <rurimoca@gmail.com>
2026-09-17 01:10:42 -0600

    ci: deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/genchord/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
